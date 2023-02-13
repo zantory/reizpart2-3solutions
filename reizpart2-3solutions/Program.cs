97a8b11ca06e8c8527ccef926ed34c4d59663133
Vassu Duggal <vassuduggal@gmail.com>
2023-02-13 19:07:59 +0200

    Add project files.

---
+++ reizpart2-3solutions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreeDepthCalculator
+{
+    class Branch
+    {
+        public List<Branch> branches { get; set; }
+
+        public Branch()
+        {
+            branches = new List<Branch>();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Branch root = new Branch();
+            root.branches.Add(new Branch());
+            root.branches[0].branches.Add(new Branch());
+            root.branches[0].branches[0].branches.Add(new Branch());
+            root.branches[0].branches[0].branches[0].branches.Add(new Branch());
+
+            Console.WriteLine("The depth of the tree is " + GetDepth(root, 0) + ".");
+            Console.ReadLine();
+        }
+
+        static int GetDepth(Branch branch, int depth)
+        {
+            int maxDepth = depth;
+
+            foreach (Branch b in branch.branches)
+            {
+                int currentDepth = GetDepth(b, depth + 1);
+                if (currentDepth > maxDepth)
+                {
+                    maxDepth = currentDepth;
+                }
+            }
+
+            return maxDepth;
+        }
+    }
+}
